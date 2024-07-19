0dbf2cdac11b7b2c805994a4c2028b17033703b0
Zibras Ismail <mohamedzibras2015@gmail.com>
2024-07-20 00:05:25 +0530

    update the root.tsx

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,15 +1,12 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
-import { generateRouter } from "./routers/generate";
 import { checkoutRouter } from "./routers/checkout";
-
+import { generateRouter } from "./routers/generate";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   generate: generateRouter,
   checkout: checkoutRouter,
 });
